21c800cecf4db58bdacb3d5e6a4db384c8b32232
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-16 16:47:36 +0200

    cambiata condizione

--- script/script.js
+++ script/script.js
@@ -2,17 +2,18 @@ const listaSpesa =["latte","cacao","biscotti","mortadella","pane","acqua","pasta
 
 let index=0;
 
-const container= document.querySelector("ul.container");
-
-let liEl =document.createElement("li");
 
 
-while(listaSpesa.length > index){
+
+while( index<listaSpesa.length){
+    const container= document.querySelector("ul.container");
+
+    let liEl =document.createElement("li");
 
     container.appendChild(document.createElement("li"));
-    
-    
 
+    liEl.append(listaSpesa[index])
+    
     index++;
 
 }
